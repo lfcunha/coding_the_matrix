410532fb884ea26f550981c3e260045508fb887f
Luis Cunha <lfcunha@gmail.com>
2013-09-09 03:05:56 -0400

    added orthonormalization from 'wukat' - did not test on grader

--- matrix-hw7/orthonormalization.py
+++ matrix-hw7/orthonormalization.py
@@ -1,14 +1,32 @@
+from math import sqrt
+from matutil import coldict2mat
 from orthogonalization import orthogonalize, aug_orthogonalize
-from math import sqrt as r
+from vec import Vec
+from vecutil import list2vec
+
+
+
 def orthonormalize(L):
     '''
     Input: a list L of linearly independent Vecs
     Output: A list T of orthonormal Vecs such that for all i in [1, len(L)],
             Span L[:i] == Span T[:i]
     '''
-    a = orthogonalize(L)
-    return [ v/sqrt(v*v) for v in a]
+    L1 = orthogonalize(L)
+    L2 = []
+    for i in L1:
+        temp = 0
+        for j in i.D:
+            temp += i[j] ** 2
+        L2.append(temp)
+    return [L1[i] / sqrt(L2[i]) for i in range(len(L1))]
 
+def adjust(v, multipliers):
+    assert set(range(len(multipliers))) == v.D
+    w = Vec(v.D, {})
+    for i in range(len(v.D)):
+        w[i] = multipliers[i]*v[i]
+    return w
 
 def aug_orthonormalize(L):
     '''
@@ -19,10 +37,14 @@ def aug_orthonormalize(L):
             * coldict2mat(L) == coldict2mat(Qlist) * coldict2mat(Rlist)
             * Qlist = orthonormalize(L)
     '''
-
-
-    Qlist = aug_orthonormalize(L)
-    Qtmat = coldict2mat(Qlist).transpose()
-    Rmat = Qtmat*coldict2mat(L)
-    Rlist = list(mat2coldict(Rmat).values())
-    return Qlist,Rlist
+    L1, L2 = aug_orthogonalize(L)
+    L3 = []
+    for i in L1:
+        temp = 0
+        for j in i.D:
+            temp += i[j] ** 2
+        L3.append(temp)
+    L3 = [sqrt(i) for i in L3]
+    Qlist = orthonormalize(L)
+    Rlist = [adjust(L2[i], L3) for i in range(len(L2))]
+    return Qlist, Rlist
